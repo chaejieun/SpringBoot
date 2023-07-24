4c7a13f8e9cfc9a1538b0bd0c38a206b4596a861
DESKTOP-FH0C408\HectonProjectP <HectonProjectP>
2023-07-24 10:11:38 +0900

    merge

--- src/main/java/tobyspring/helloboot/HelloController.java
+++ src/main/java/tobyspring/helloboot/HelloController.java
@@ -3,8 +3,15 @@ package tobyspring.helloboot;
 import java.util.Objects;
 
 public class HelloController {
+	
+	private final HelloService helloService;
+	
+	public HelloController(HelloService helloService) {
+		this.helloService = helloService;
+	}
+
 	public String hello(String name) {
-		SimpleHelloService helloService = new SimpleHelloService();
+		//SimpleHelloService helloService = new SimpleHelloService();
 		
 		return helloService.sayHello(Objects.requireNonNull(name));	// null인 경우 방지 처리 해주는 메소드
 	}
--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -1,7 +1,6 @@
 package tobyspring.helloboot;
 
 import java.io.IOException;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -15,12 +14,15 @@ import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 @SpringBootApplication
 public class HellobootApplication {
 
 	public static void main(String[] args) {
-		GenericApplicationContext applicationContext = new GenericApplicationContext();
+		GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
+		applicationContext.registerBean(SimpleHelloService.class);
 		applicationContext.registerBean(HelloController.class);
 		applicationContext.refresh();
 		
@@ -28,26 +30,8 @@ public class HellobootApplication {
 		
 		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
 		WebServer webServer = serverFactory.getWebServer(ServletContext -> {
-			ServletContext.addServlet("frontcontroller", new HttpServlet() {
-				@Override
-				protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-					// 인증, 보안, 다국어, 공통 기능
-					if(req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
-						String name = req.getParameter("name");
-						
-						//resp.setStatus(HttpStatus.OK.value());
-						//resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
-						
-						HelloController helloController = applicationContext.getBean(HelloController.class);
-						String ret = helloController.hello(name);
-						
-						resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
-						resp.getWriter().println(ret);
-					}else {
-						resp.setStatus(HttpStatus.NOT_FOUND.value());
-					}
-				}
-			}).addMapping("/*");
+			ServletContext.addServlet("dispatcherServlet",new DispatcherServlet(applicationContext)
+					).addMapping("/*");
 		});
 		
 		webServer.start();
