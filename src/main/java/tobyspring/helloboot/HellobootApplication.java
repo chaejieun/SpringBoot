40eca96ac0457732706a7c00c7ef1c60b8d591a6
chaejieun <pettojim@naver.com>
2023-07-24 21:34:09 +0900

    feat: 스프링 컨테이너로 통합

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -11,20 +11,28 @@ import org.springframework.web.servlet.DispatcherServlet;
 public class HellobootApplication {
 
 	public static void main(String[] args) {
-		GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
+		GenericWebApplicationContext applicationContext = new GenericWebApplicationContext() {
+
+			@Override
+			protected void onRefresh() {
+				// TODO Auto-generated method stub
+				super.onRefresh();
+				
+				ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+				WebServer webServer = serverFactory.getWebServer(ServletContext -> {
+					ServletContext.addServlet("dispatcherServlet",new DispatcherServlet(this)
+							).addMapping("/*");
+				});
+				
+				webServer.start();
+			}
+		
+		};
+		
 		applicationContext.registerBean(SimpleHelloService.class);
 		applicationContext.registerBean(HelloController.class);
 		applicationContext.refresh();
 		
-		//SpringApplication.run(HellobootApplication.class, args);
-		
-		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-		WebServer webServer = serverFactory.getWebServer(ServletContext -> {
-			ServletContext.addServlet("dispatcherServlet",new DispatcherServlet(applicationContext)
-					).addMapping("/*");
-		});
-		
-		webServer.start();
 	}
 
 }
