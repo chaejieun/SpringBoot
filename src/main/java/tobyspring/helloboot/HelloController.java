5649f41cd08ffb951115e2cf3cf980262b7c7352
chaejieun <pettojim@naver.com>
2023-07-24 22:03:39 +0900

    feat: @Component 스캔

--- src/main/java/tobyspring/helloboot/HelloController.java
+++ src/main/java/tobyspring/helloboot/HelloController.java
@@ -5,8 +5,10 @@ import java.util.Objects;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/hello")
+@RestController
 public class HelloController {
 	
 	private final HelloService helloService;
@@ -16,7 +18,6 @@ public class HelloController {
 	}
 
 	@GetMapping
-	@ResponseBody
 	public String hello(String name) {
 		return helloService.sayHello(Objects.requireNonNull(name));	// null인 경우 방지 처리 해주는 메소드
 	}
