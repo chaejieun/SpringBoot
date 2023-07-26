d801d17294bf56b4813b3859c0fd3876f2e187dc
DESKTOP-FH0C408\HectonProjectP <HectonProjectP>
2023-07-26 17:09:13 +0900

    feat: 자동 구성 정보 파일 분리

--- src/main/java/config/MyAutoConfigImportSelector.java
+++ src/main/java/config/MyAutoConfigImportSelector.java
@@ -1,16 +1,38 @@
 package config;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.boot.context.annotation.ImportCandidates;
 import org.springframework.context.annotation.DeferredImportSelector;
 import org.springframework.core.type.AnnotationMetadata;
 
 public class MyAutoConfigImportSelector implements DeferredImportSelector{
+	
+	private final ClassLoader classLoader;
+	
+	public MyAutoConfigImportSelector(ClassLoader classLoader) {
+		this.classLoader = classLoader;
+		
+	}
 
 	@Override
 	public String[] selectImports(AnnotationMetadata importingClassMetadata) {
-		return new String[] {
-				"tobyspring.config.config.DispatcherServletConfg",
-				"tobyspring.config.config.TomcatWebServerConfig",
-		};
+		
+		List<String> autoConfigs = new ArrayList<>();
+		
+//		for(String candidate: ImportCandidates.load(MyAutoConfiguration.class, classLoader)) {
+//			autoConfigs.add(candidate);
+//		}
+		
+		ImportCandidates.load(MyAutoConfiguration.class, classLoader).forEach(candidate -> 
+					autoConfigs.add(candidate)
+				);
+		
+		return autoConfigs.toArray(new String[0]);
+		
 	}
+
+
 
 }
