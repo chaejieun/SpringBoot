f841fdf5ce8c05ec59365e5b6a9d107554262c78
DESKTOP-FH0C408\HectonProjectP <HectonProjectP>
2023-07-26 17:36:52 +0900

    feat: 자동 구성 애노테이션 적용

--- src/main/java/config/autoconfig/DispathcerServletConfig.java
+++ src/main/java/config/autoconfig/DispathcerServletConfig.java
@@ -4,7 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.DispatcherServlet;
 
-@Configuration
+import config.MyAutoConfiguration;
+
+@MyAutoConfiguration
 public class DispathcerServletConfig {
 
 
--- src/main/java/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/config/autoconfig/TomcatWebServerConfig.java
@@ -5,7 +5,9 @@ import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+import config.MyAutoConfiguration;
+
+@MyAutoConfiguration
 public class TomcatWebServerConfig {
 
 	@Bean
